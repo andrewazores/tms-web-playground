1f19affc97ffdc98662477e01afc0e240e4bcb3e
Andrew Azores <me@andrewazor.es>
2017-03-17 12:53:10 -0400

    Use card view with kebab dropdown

--- src/vm-cpu/vm-cpu-controller.js
+++ src/vm-cpu/vm-cpu-controller.js
@@ -2,5 +2,6 @@ angular.module('apf.vmCpuModule').controller('vmCpuController', ['$scope',
   function ($scope) {
     'use strict';
     $scope.vmName = 'FooVm';
+    $scope.dropdownActions = ['Action 1', 'Action 2'];
   }
 ]);
