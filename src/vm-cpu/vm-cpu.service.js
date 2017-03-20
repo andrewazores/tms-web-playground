cc431cb648acccc4107c3172150057ae869ab1f0
Andrew Azores <me@andrewazor.es>
2017-03-20 12:56:29 -0400

    Conditionally supply generated cpu data in vm-cpu.service

--- src/vm-cpu/vm-cpu.service.js
+++ src/vm-cpu/vm-cpu.service.js
@@ -1,11 +1,35 @@
-angular
-  .module('apf.vmCpuModule')
-  .factory('CpuStats', ['thermostatWebServerUrl', '$resource',
-    function (thermostatWebServerUrl, $resource) {
-      'use strict';
-      return $resource(thermostatWebServerUrl + '/cpu-stats/latest', {}, {
-        query: {
-          method: 'GET'
-        }
-      });
-    }]);
+var useLiveData = false;
+if (useLiveData) {
+  angular
+    .module('apf.vmCpuModule')
+    .factory('CpuStats', ['thermostatWebServerUrl', '$resource',
+      function (thermostatWebServerUrl, $resource) {
+        'use strict';
+        return $resource(thermostatWebServerUrl + '/cpu-stats/latest', {}, {
+          query: {
+            method: 'GET'
+          }
+        });
+      }]);
+} else {
+  angular
+    .module('apf.vmCpuModule')
+    .factory('CpuStats', ['thermostatWebServerUrl', '$resource',
+      function (thermostatWebServerUrl, $resource) {
+        'use strict';
+        return {
+          query: function (config, onSuccess, onFailure) {
+            onSuccess({
+              response: [
+                {
+                  timeStamp: {
+                    $numberLong: Date.now()
+                  },
+                  perProcessorUsage: [ Math.random() * 100 ]
+                }
+              ]
+            });
+          }
+        };
+      }]);
+}
