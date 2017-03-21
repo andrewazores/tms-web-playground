5b86a6abe1e429bf76eae84eb0e63adb4b5ed994
Andrew Azores <me@andrewazor.es>
2017-03-21 15:57:54 -0400

    Use basic bar chart card for vm-memory

--- src/vm-memory/memory/memory-controller.js
+++ src/vm-memory/memory/memory-controller.js
@@ -4,8 +4,31 @@ angular
     function ($scope, MemoryService) {
       'use strict';
       MemoryService.query({}, function success (result) {
-        $scope.data = JSON.parse(result.message).response;
-        console.log($scope.data);
+        var response = JSON.parse(result.message).response;
+        var isGenerational = response.length && angular.isDefined(response[0].generations);
+        if (isGenerational) {
+          $scope.templateUrl = 'src/vm-memory/memory/generational.html';
+          $scope.layoutInline = { type: 'inline' };
+          $scope.data = [];
+          for (var i = 0; i < response.length; i++) {
+            var gens = response[i].generations;
+            for (var j = 0; j < gens.length; j++) {
+              var g = gens[j];
+              for (var k = 0; k < g.spaces.length; k++) {
+                var space = g.spaces[k];
+                $scope.data.push({
+                  title : space.name,
+                  data : {
+                    used : parseInt(space.used.$numberLong),
+                    total : parseInt(space.capacity.$numberLong)
+                  }
+                });
+              }
+            }
+          }
+        } else {
+          $scope.templateUrl = 'src/vm-memory/memory/generic.html';
+        }
       });
 
       $scope.parseDate = function (timeStamp) {
